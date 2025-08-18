0c8b0cfad3cc2f0cd48d3ff6dacc8ed03087a793
Ouail Bezzahi <s106674@ap.be>
2025-08-18 02:06:07 +0200

    coin animation debugging

--- Items/Coin/AnimatedCoin.cs
+++ Items/Coin/AnimatedCoin.cs
@@ -1,28 +1,45 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace ForestQuest.Items.Coin
 {
+    // Simple sprite-sheet animation styled like Player / Enemy:
+    // - Evenly sliced horizontal frames
+    // - Fixed frame time loop (always animates)
+    // - Scaled draw + simple BoundingBox
     public class AnimatedCoin
     {
         private Texture2D _spriteSheet;
         private Vector2 _position;
-        private int _frameWidth = 120;
-        private int _frameHeight = 296;
+
+        private int _frameWidth;
+        private int _frameHeight;
+        private int _currentFrame;
         private int _frameCount = 7;
-        private int _currentFrame = 0;
-        private float _frameTime = 0.1f; // tijd per frame
-        private double _elapsedTime = 0;
-        private float _scale = 0.15f; // mooie grootte voor coin
+
+        private double _frameTime = 0.10;
+        private double _elapsedTime;
+
+        private float _scale = 0.15f;
 
         public Vector2 Position => _position;
-        public Rectangle BoundingBox => new Rectangle((int)_position.X, (int)_position.Y, _frameWidth, _frameHeight);
+        public int DrawWidth => (int)(_frameWidth * _scale);
+        public int DrawHeight => (int)(_frameHeight * _scale);
+        public Rectangle BoundingBox => new Rectangle((int)_position.X, (int)_position.Y, DrawWidth, DrawHeight);
 
         public AnimatedCoin(ContentManager content, Vector2 position)
         {
-            _spriteSheet = content.Load<Texture2D>("Items/coin");
             _position = position;
+            LoadContent(content);
+        }
+
+        private void LoadContent(ContentManager content)
+        {
+            _spriteSheet = content.Load<Texture2D>("Items/coin");
+            _frameWidth = _spriteSheet.Width / _frameCount;
+            _frameHeight = _spriteSheet.Height;
         }
 
         public void Update(GameTime gameTime)
@@ -30,17 +47,28 @@ namespace ForestQuest.Items.Coin
             _elapsedTime += gameTime.ElapsedGameTime.TotalSeconds;
             if (_elapsedTime >= _frameTime)
             {
-                _currentFrame++;
                 _elapsedTime = 0;
-                if (_currentFrame >= _frameCount) _currentFrame = 0;
+                _currentFrame++;
+                if (_currentFrame >= _frameCount)
+                    _currentFrame = 0;
             }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             Rectangle sourceRect = new Rectangle(_currentFrame * _frameWidth, 0, _frameWidth, _frameHeight);
-            Vector2 origin = Vector2.Zero;
-            spriteBatch.Draw(_spriteSheet, _position, sourceRect, Color.White, 0f, origin, _scale, SpriteEffects.None, 0f);
+
+            spriteBatch.Draw(
+                _spriteSheet,
+                _position,
+                sourceRect,
+                Color.White,
+                0f,
+                Vector2.Zero,
+                _scale,
+                SpriteEffects.None,
+                0f
+            );
         }
     }
-} 
+}
